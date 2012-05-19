ae22f4de972941b8522c23a2b5369bc2e5e92eb9
Brendan Benson <brendanbenson@gmail.com>
2012-05-19 13:21:20 -0600

    Added meta description to home page for SERPs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   
   def home
     @title = 'Logo Quiz Game!'
+    @description = 'Test your knowledge of corporate logos in this fast-paced logo quiz! How well do you know brands like Nike, Pepsi, and CNN?'
   end
 
   def contact
