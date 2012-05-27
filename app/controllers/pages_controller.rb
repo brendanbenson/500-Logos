f4896fea755d6f9f085be7faf3c4f9b5b8d6f3c2
Brendan Benson <brendanbenson@gmail.com>
2012-05-27 13:46:27 -0600

    Remove about page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,13 +8,5 @@ class PagesController < ApplicationController
   def contact
     @title = 'Contact'
   end
-  
-  def about
-    @title = 'About'
-  end
-  
-  def help
-    @title = 'Help'
-  end
 
 end
