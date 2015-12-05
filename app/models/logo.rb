92284c5f367a7b47b5e1780977dededf888e123a
Brendan Benson <brendanbenson@gmail.com>
2015-12-04 22:42:24 -0700

    Logo import script

--- app/models/logo.rb
+++ app/models/logo.rb
@@ -1,2 +1,9 @@
+require 'csv'
+
 class Logo < ActiveRecord::Base
+  def self.import(filename)
+    CSV.foreach(filename, headers: true) do |line|
+      Logo.create!(line.to_hash)
+    end
+  end
 end
